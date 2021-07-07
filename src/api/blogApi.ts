9c5d410df1dc197a8de9b4931e9168c14b310a98
Oleh Pohorilyy <olegpogorili.op@gmail.com>
2021-07-07 17:37:51 +0100

    project setup

---
+++ src/api/blogApi.ts
@@ -0,0 +1,7 @@
+import axios from 'axios'
+
+class BlogApiClient {}
+
+const client = new BlogApiClient()
+
+export default client
---
+++ src/pages/index.tsx
@@ -0,0 +1,14 @@
+import Head from 'next/head'
+
+export default function LatestPosts() {
+  return (
+    <>
+      <Head>
+        <title>Next.JS Blog</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <main></main>
+    </>
+  )
+}
